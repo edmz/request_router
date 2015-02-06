bc72bda55d0821930499a1495345c88d6703b115
Chris McCord <chris@chrismccord.com>
2015-02-06 14:22:33 -0500

    Add poolboy/feedback

--- lib/accounter/request_supervisor.ex
+++ lib/accounter/request_supervisor.ex
@@ -2,15 +2,25 @@ defmodule Accounter.RequestSupervisor do
   use Supervisor
   require Logger
 
-  def start_link do
-    Logger.info "Starting supervisor."
-    Supervisor.start_link(__MODULE__, [])
-  end  
+  @pool_size 50 # Max number of simult requests
 
-  def init(_) do
-    children = [
-      worker(Accounter.Webservice, [], restart: :transient)
+  def start_link(opts \\ []) do
+    Logger.info "Starting supervisor."
+    Supervisor.start_link(__MODULE__, [opts])
+  end
+
+  def init(opts) do
+    pool_opts = [
+      name: {:local, :webservice_pool},
+      worker_module: Accounter,Webservice,
+      size: opts[:pool_size] || @pool_size,
+      max_overflow: 0
     ]
-    supervise(children, strategy: :simple_one_for_one)    
+
+    children = [
+      :poolboy.child_spec(:webservice_pool, pool_opts, []),
+    ]
+
+    supervise(children, strategy: :simple_one_for_one)
   end
 end
--- lib/accounter/webservice.ex
+++ lib/accounter/webservice.ex
@@ -3,30 +3,48 @@ defmodule Accounter.Webservice do
   require Logger
   require HTTPoison
 
+
+  @doc """
+  Fetch a URL asynchronously and post results to an endpoint
+
+    * `fetch_url` - The URL to fetch
+    * `post_to_url` - The URL to POST results back to
+
+  ## Examples
+
+      iex> async_query("example.com", "myendpoint.com")
+      :ok
+
+  """
+  def async_query(fetch_url, post_to_url) do
+    ws_pid = :poolboy.checkout(:webservice_pool)
+    Task.start fn ->
+      :ok = GenServer.call(ws_pid, {:query, fetch_url, post_to_url})
+      :poolboy.checkin(ws_pid)
+    end
+  end
+
+  def start_link() do
+    Logger.info "Starting webservice."
+    GenServer.start_link(__MODULE__, [])
+  end
+
   def init(_) do
     Logger.info "Init de Webservice"
     {:ok, nil}
   end
 
-  def start_link() do
-    Logger.info "Starting webservice."
-    GenServer.start_link(__MODULE__, nil, name: :webservice)
-  end  
-
-  def handle_cast({:query, _}, _) do
+  def handle_call({:query, fetch_url, post_to_url}, from, state) do
     Logger.info "handling call"
     {:ok, response} = HTTPoison.get "http://httpbin.org/user-agent"
+    # TODO need to handle bad response
     Logger.info response.body
     HTTPoison.post "http://0.0.0.0:4000/", ""
-    {:noreply, response.body}
+    {:reply, :ok, state}
   end
 
-  def handle_cast(a, b) do
+  def handle_call(msg, _from, state) do
     Logger.info "Handling whatever"
-  end
-
-  def server_process() do
-    Accounter.Webservice.start_link
-    GenServer.cast(:webservice, {:query, "bleh"})
+    {:noreply, state}
   end
 end
